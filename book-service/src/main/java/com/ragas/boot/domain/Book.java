cda68eb2eb350964c5e913ae517d58372144be92
Chandra Sekhar JAGARLAMUDI <chandra.jagarlamudi@gmail.com>
2017-12-12 15:54:00 -0600

    Initial commit

---
+++ book-service/src/main/java/com/ragas/boot/domain/Book.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.ragas.boot.domain;
+
+/**
+ * @author Chandra Jagarlamudi
+ *
+ */
+public class Book {
+	private long id;
+	private String title;
+	private String author;
+	private String description;
+	private String summary;
+	
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the summary
+	 */
+	public String getSummary() {
+		return summary;
+	}
+	/**
+	 * @param summary the summary to set
+	 */
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	
+}
