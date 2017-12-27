0724f41f144e3356483700a3de8afbb9fab6fa8c
Chandra Jagarlamudi <chandra.jagarlamudi@gmail.com>
2017-12-26 22:17:55 -0600

    Added service discovery code

--- spring-cloud/currency-converter/currency-conversion-service/src/main/java/com/ragas/springcloud/microservice/CurrencyConversionServiceApplication.java
+++ spring-cloud/currency-converter/currency-conversion-service/src/main/java/com/ragas/springcloud/microservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.ragas.springcloud.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.ragas.springcloud.microservice.service")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- spring-cloud/currency-converter/currency-conversion-service/src/main/java/com/ragas/springcloud/microservice/service/CurrencyExchangeServiceProxy.java
+++ spring-cloud/currency-converter/currency-conversion-service/src/main/java/com/ragas/springcloud/microservice/service/CurrencyExchangeServiceProxy.java
@@ -1,6 +1,7 @@
 package com.ragas.springcloud.microservice.service;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,7 +12,8 @@ import com.ragas.springcloud.microservice.persistence.model.CurrencyConverter;
  *
  */
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
