6a2705b5348b26b854b068c9da92ac2d81123e2e
Chandra Jagarlamudi <chandra.jagarlamudi@gmail.com>
2017-12-26 20:05:18 -0600

    Initial commit

---
+++ spring-cloud/currency-converter/currency-conversion-service/src/main/java/com/ragas/springcloud/microservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,14 @@
+package com.ragas.springcloud.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.ragas.springcloud.microservice.service")
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
