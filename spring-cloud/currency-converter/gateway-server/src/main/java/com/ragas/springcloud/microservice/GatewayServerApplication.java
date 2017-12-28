471120706a0e6db6c510cb0984857aeee935369a
Chandra Jagarlamudi <chandra.jagarlamudi@gmail.com>
2017-12-27 20:54:09 -0600

    Added gateway server

---
+++ spring-cloud/currency-converter/gateway-server/src/main/java/com/ragas/springcloud/microservice/GatewayServerApplication.java
@@ -0,0 +1,16 @@
+package com.ragas.springcloud.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+}
