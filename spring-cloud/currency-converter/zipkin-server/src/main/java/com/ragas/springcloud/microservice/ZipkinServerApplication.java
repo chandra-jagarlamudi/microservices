d56b2f59669376bf0271f9297853c3678a064ae6
Chandra Jagarlamudi <chandra.jagarlamudi@gmail.com>
2018-01-01 16:48:43 -0600

    Cleaned POM files and enabled discovery client for Zipkin server

--- spring-cloud/currency-converter/zipkin-server/src/main/java/com/ragas/springcloud/microservice/ZipkinServerApplication.java
+++ spring-cloud/currency-converter/zipkin-server/src/main/java/com/ragas/springcloud/microservice/ZipkinServerApplication.java
@@ -2,7 +2,12 @@ package com.ragas.springcloud.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZipkinServerApplication {
 
