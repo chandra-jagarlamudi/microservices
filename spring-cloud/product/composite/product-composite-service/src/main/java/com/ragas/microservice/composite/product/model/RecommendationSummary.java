6fa8e75b3b8c460ebfa22c63211863db25211169
Chandra Jagarlamudi <chandra.jagarlamudi@aa.com>
2018-01-30 12:27:30 -0600

    product-composite-service initial commit

---
+++ spring-cloud/product/composite/product-composite-service/src/main/java/com/ragas/microservice/composite/product/model/RecommendationSummary.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.ragas.microservice.composite.product.model;
+
+/**
+ * @author Chandra Jagarlamudi
+ *
+ */
+public class RecommendationSummary {
+
+	private int recommendationId;
+	private String author;
+	private int rate;
+
+	public RecommendationSummary(int recommendationId, String author, int rate) {
+		this.recommendationId = recommendationId;
+		this.author = author;
+		this.rate = rate;
+	}
+
+	public int getRecommendationId() {
+		return recommendationId;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+}
