05dab0c806b61f864691aa7854a22cd7bbf4d361
Chandra Jagarlamudi <chandra.jagarlamudi@aa.com>
2018-02-09 19:42:56 -0600

    Deleted project

--- spring-cloud/product/build-all.bat
+++
@@ -1,7 +0,0 @@
-pushd core\product-service &                call mvn clean install & popd
-pushd core\recommendation-service &         call mvn clean install & popd
-pushd core\review-service &                 call mvn clean install & popd
-pushd composite\product-composite-service & call mvn clean compile & popd
-
-pushd support\discovery-server &            call mvn clean compile & popd
-pushd support\edge-server &                 call mvn clean compile & popd
